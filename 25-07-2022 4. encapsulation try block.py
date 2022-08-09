9033e696dd83b41205952aa53b5c9eb727d0117d
goyalnishu <99133253+goyalnishu@users.noreply.github.com>
2022-08-08 21:24:49 -1200

    Create 25-07-2022 4. encapsulation try block.py

---
+++ 25-07-2022 4. encapsulation try block.py
@@ -0,0 +1,21 @@
+class invalidnumber():
+
+    def __init__(self,arg):
+        self.arg=arg
+
+try:
+    num=int(input())
+    if num>20:
+        print("hello world")
+    else:
+        print(num)
+
+except :
+    print("alok kumar")
+
+
+
+
+
+
+
